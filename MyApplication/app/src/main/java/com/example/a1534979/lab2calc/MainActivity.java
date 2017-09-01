586db2761fb925263db08c7c13e01a0b11335553
Fool <mtahar1998@gmail.com>
2017-09-01 16:44:08 -0400

    Renewal

---
+++ MyApplication/app/src/main/java/com/example/a1534979/lab2calc/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.a1534979.lab2calc;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
