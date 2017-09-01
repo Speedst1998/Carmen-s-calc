e389ac4fe4469a882821c17c25825d1e1e7a425e
Fool <mtahar1998@gmail.com>
2017-09-01 18:01:38 -0400

    Kinda done

--- MyApplication/app/src/main/java/com/example/a1534979/lab2calc/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/a1534979/lab2calc/MainActivity.java
@@ -1,23 +1,44 @@
 package com.example.a1534979.lab2calc;
 
 import android.support.v7.app.AppCompatActivity;
+import android.widget.*;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
     EditText etAmount, etTerm, etInterest;
-    TextView result;
+    TextView amountR, termR, interestR;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
+        etAmount = (EditText) findViewById(R.id.amount);
+        etTerm = (EditText) findViewById(R.id.term);
+        etInterest = (EditText) findViewById(R.id.interest);
+        amountR = (TextView) findViewById(R.id.amountR);
+        termR = (TextView) findViewById(R.id.termR);
+        interestR = (TextView) findViewById(R.id.interestR);
+
     }
 
-    public void clearNums(View v){
+    public void clear(View v) {
 
-        etNum1.setText("");
-        etNum2.setText("");
+        etAmount.setText("");
+        etTerm.setText("");
+        etInterest.setText("");
 
+    }
+
+    public void calculate(View v) {
+
+
+        LoanCalculator cal = new LoanCalculator(Double.parseDouble(etAmount.getText().toString()),
+                Integer.parseInt(etTerm.getText().toString()),
+                Double.parseDouble(etInterest.getText().toString()));
+        amountR.setText(cal.getLoanAmount() + "");
+        termR.setText(cal.getMonthlyPayment() + "");
+        interestR.setText(cal.getNumberOfYears() + "");
     }
 }
